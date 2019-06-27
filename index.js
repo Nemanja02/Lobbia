62db6f4fc44383f9ca283fed7ebffab6eb4d0380
pavles6 <pavles6@protonmail.com>
2019-06-27 02:13:20 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const next_config = require("./config/next.config");
+
+const app = next({
+  dev
+});
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(next_config.port, err => {
+      if (err) throw err;
+      console.log(
+        "\n\t\t\x1b[46m%s\x1b[0m\n",
+        `Server running on http://localhost:${next_config.port}`
+      );
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
