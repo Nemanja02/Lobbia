e04ed07193fd97c42fa0e769ed8fbff46ee07e6c
VodnikAngus <petar.radojevic22@gmail.com>
2019-07-04 21:01:32 +0200

    small changes

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -68,7 +68,7 @@ module.exports = {"container":"_3YXQ9eM8q71zIRlzguFU8o","navHeading":"_1rBufVove
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1562251699818");
+            injectCss(link, link.href.split("?")[0] + "?unix=1562266837018");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1e1bd771a57f3f8ac548";
+/******/ 	var hotCurrentHash = "ac276dfdccd655e0996e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
