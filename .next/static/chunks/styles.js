87f164c415930a67ccd9965e22b97338f5a4267e
Petar <petar@pop-os.localdomain>
2019-07-07 01:22:37 +0200

    test

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -30,7 +30,7 @@ module.exports = {"root":"_3kwh7x4ISw2sdc56HmDvZN","main":"_1vO7nIIcRqZ9HZwtzTFd
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1562189405350");
+            injectCss(link, link.href.split("?")[0] + "?unix=1562455348859");
           });
       }
     }
@@ -68,7 +68,7 @@ module.exports = {"container":"_137uQMmYZ9psIw-Unn1Z-W","navHeading":"UB3gAnnnnk
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1562189405411");
+            injectCss(link, link.href.split("?")[0] + "?unix=1562455348939");
           });
       }
     }
@@ -106,7 +106,7 @@ module.exports = {"h1":"_1m-yaVkZe3V6us4CJcB9OD","h2":"WI2ZzuYunNKUnl6SRJhdl","h
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1562189405343");
+            injectCss(link, link.href.split("?")[0] + "?unix=1562455348852");
           });
       }
     }
@@ -144,7 +144,7 @@ module.exports = {"wrapper":"IoDNnoLrVbc1VFd4LBVVZ"};;
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1562189405328");
+            injectCss(link, link.href.split("?")[0] + "?unix=1562455348843");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "db64939d5627ee23700f";
+/******/ 	var hotCurrentHash = "742ac062a010e4af20d3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
