a501ffcdbb28ed02e80fa84358670c0a285fa578
VodnikAngus <petar.radojevic22@gmail.com>
2019-07-12 23:03:42 +0200

    Added basic register page (No styles applied)

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a5baf173228ef3f8fcfe";
+/******/ 	var hotCurrentHash = "86be8c80afa21fbd22ec";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
