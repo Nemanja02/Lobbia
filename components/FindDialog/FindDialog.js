2d67176edcbc26cb81dc0f5a650b7ee8b48d66e1
VodnikAngus <petar.radojevic22@gmail.com>
2019-07-20 16:30:08 +0200

    Find dialog changes

--- components/FindDialog/FindDialog.js
+++ components/FindDialog/FindDialog.js
@@ -4,22 +4,29 @@ import classes from "./Find.module.scss";
 function Layout(props) {
   return (
     <div className={classes.findBG}>
-        <div className={classes.dialog}>
-            <div>
-                <div />
-                <span>Find 1 friend</span>
-            </div>
-            <div>
-                <div />
-                <span>Find 2 friends</span>
-            </div>
-            <div>
-                <div />
-                <span>Find 4 friends</span>
-            </div>
-        </ div>
+      <div className={classes.dialog}>
+        <a>
+          <div className={classes.grow} />
+          <div>
+            <span>Find 1 friend</span>
+          </div>
+        </a>
+        <a>
+          <div className={classes.grow} />
+          <div>
+            <span>Find 2 friends</span>
+          </div>
+        </a>
+        <a>
+          <div className={classes.grow} />
+          <div>
+            <span>Find 4 friends</span>
+          </div>
+        </a>
+        <i className="fas fa-times" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Layout;
