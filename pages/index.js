6fffdc9264fc41e3e6287e40d726d16536b0ecf6
VodnikAngus <petar.radojevic22@gmail.com>
2019-08-04 23:31:23 +0200

    Landin page nav

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import classes from "./styles/Index.module.scss";
 
 export class index extends Component {
   render() {
-    return <div>Hello from landing page.</div>;
+    return (
+      <>
+        <div className={classes.nav}>
+          <img src="assets/PixelArt.png" />
+
+          <nav>
+            <ul>
+              <li className={classes.active}>Home<div /></li>
+              <li>About<div /></li>
+              <li>Support<div /></li>
+            </ul>
+          </nav>
+
+          <div className={classes.buttons}>
+            <a href="/register">Sign up</a>
+            <a href="/login" className={classes.primary}>log in</a>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
