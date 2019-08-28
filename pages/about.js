229da4d7b51a38ce8e4ed98c48ccbfd2ed2bae22
Zarkozver <49551105+Zarkozver@users.noreply.github.com>
2019-08-28 22:19:46 +0200

    git pull

--- pages/about.js
+++ pages/about.js
@@ -1,29 +1,25 @@
 import React, { Component } from "react";
-import classes from "./styles/Index.module.scss";
+import classes from "./styles/About.module.scss";
 import Header from "../components/Header/Header";
 import { Typography, TextField, Container, CardMedia } from "@material-ui/core";
 import { typography, textAlign } from "@material-ui/system";
 export class about extends Component {
-  inputStyle = {
-    width: "calc(100% - 20px)",
-    margin: "10px"
-  };
-  paragraphStyle = {
-    color: "white",
-    fontSize: "24px",
-    width: "50%",
-    margin: "10px"
-  };
-  imgStyle = {
-    width: "65%",
-    float: "right",
-    margin: "10px"
-  };
-  titleStyle = {
-    color: "white",
-    fontSize: "36px",
-    margin: "10px"
-  };
+  // paragraphStyle = {
+  //   color: "white",
+  //   fontSize: "24px",
+  //   width: "50%",
+  //   margin: "10px"
+  // };
+  // imgStyle = {
+  //   width: "65%",
+  //   float: "right",
+  //   margin: "10px"
+  // };
+  // titleStyle = {
+  //   color: "white",
+  //   fontSize: "36px",
+  //   margin: "10px"
+  // };
   render() {
     return (
       <>
@@ -32,37 +28,30 @@ export class about extends Component {
           <Container maxWidth="lg" fixed>
             <br />
             {/* Neki quote nesto nmp */}
-            <Typography
-              variant="h2"
-              style={{ color: "white", textAlign: "center", fontSize: "45px" }}
-            >
+            <Typography variant="h2" className={classes.quote}>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo,
               nostrum.
             </Typography>
             <br />
             <br />
             {/* Neki naslov fzn o nama ili nesto tako(nije obavezno) */}
-            <Typography variant="subtitle1" style={this.titleStyle}>
-              Paragraph Title
-            </Typography>
+            <div className={classes.title}>Paragraph title</div>
             {/* Neki tekst/opis */}
-            <Typography style={this.paragraphStyle}>
+            <p className={classes.info}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
               ipsam maiores eaque dolorem beatae inventore sunt, sequi officiis
               earum quis. Enim facere mollitia pariatur cupiditate culpa est
               explicabo. Quaerat, eum.
-            </Typography>
+            </p>
             {/* Slika cisto da bude nesto */}
             <img
               src="https://images.pexels.com/photos/264706/pexels-photo-264706.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-              style={this.imgStyle}
+              className={classes.photo}
             />
             <div style={{ clear: "both" }}></div>
             {/* I ovo samo ako treba jos nesto da se doda */}
-            <Typography variant="subtitle1" style={this.titleStyle}>
-              Paragraph Title
-            </Typography>
-            <Typography style={this.paragraphStyle}>
+            <div className={classes.title}>Paragraph title</div>
+            <div className={classes.info}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
               ipsam maiores eaque dolorem beatae inventore sunt, sequi officiis
               earum quis. Enim facere mollitia pariatur cupiditate culpa est
@@ -70,10 +59,10 @@ export class about extends Component {
               adipisicing elit. Animi ipsam maiores eaque dolorem beatae
               inventore sunt, sequi officiis earum quis. Enim facere mollitia
               pariatur cupiditate culpa est explicabo. Quaerat, eum.
-            </Typography>
+            </div>
             <img
               src="https://images.pexels.com/photos/264706/pexels-photo-264706.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-              style={this.imgStyle}
+              className={classes.photo}
             />
           </Container>
         </div>
